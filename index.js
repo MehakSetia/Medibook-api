ebe9887031a30bd7bf0a249f1a5c66f319f9229f
MehakSetia <156164684+MehakSetia@users.noreply.github.com>
2026-07-24 10:30:44 +0530

    fixed index.js

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 require('dotenv').config();
 const express=require('express');
+const helmet=require('helmet');
+const rate=require('express-rate-limit');
+const limiter=rate({
+  windowMs:15*60*1000,
+  max:100
+});
 const setupSwagger = require('./config/swagger');
 const app=express();
 setupSwagger(app);
 app.use(express.json());
+app.use(helmet());
 const doctorRoutes=require('./routes/doctors');
-const patientRoutes=require('./routes/patients');
 const appointmentRoutes=require('./routes/appointments');
 require('./jobs/cron');
 const authRoutes=require('./routes/auth');
@@ -13,11 +19,14 @@ const scheduleRoutes=require('./routes/schedule');
 const cors = require('cors');
 app.use(cors());
 
+
 app.use('/doctors',doctorRoutes);
-app.use('/patients',patientRoutes);
 app.use('/appointments',appointmentRoutes);
 app.use('/auth',authRoutes);
 app.use('/schedule',scheduleRoutes);
+
+const errorHandler=require('./middleware/errorHandler');
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
